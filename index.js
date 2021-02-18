1cadcc5f6448921ce022e8dcb8f88a3237cd78ca
logotipiwe <german.reus@bk.ru>
2021-02-18 11:33:51 +0300

    мобилка

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ window.onload = ()=>{
             console.log(i, screenTopPosInWindow.toFixed(2));
             const animateWindowPos = 0.3; //до какой части экрана должен дойти верх блока чтобы его анимировало
             if(screenTopPosInWindow > animateWindowPos && !screen.className.includes(animClass)) {
-                const animType = Math.ceil(Math.random()*4);
                 screen.setAttribute('class', [...screen.classList, 'anim'].join(' '));
             }
         })
