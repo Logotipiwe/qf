c5a90a9543ccbb862b902e352843e838803983fc
logotipiwe <german.reus@bk.ru>
2021-02-25 18:44:17 +0300

    Модалки в дешборд

--- dashboard/index_v2.js
+++ dashboard/index_v2.js
@@ -18,5 +18,30 @@ window.onload = ()=>{
         })
     })
 
+    const showModalFunc = modalId =>{
+        const openedClass = 'modal_opened';
+        const transitionTime = 400;
+        const modal = document.getElementById(modalId);
+        if(modal.className.includes(openedClass)){
+            modal.className = modal.className.replace(openedClass, '');
+            setTimeout(()=>{
+                modal.style.display = 'none';
+            }, transitionTime)
+        } else {
+            modal.style.display = 'flex';
+            setTimeout(()=>{
+                modal.className += ' ' + openedClass;
+            })
+        }
+    }
+
+    document.getElementById('add_new_btn_1').onclick = showModalFunc.bind(null, 'modal_1')
+    document.getElementById('add_new_btn_2').onclick = showModalFunc.bind(null, 'modal_2')
+    Array.from(document.getElementsByClassName('modal')).forEach(modal=>{
+        modal.onclick = e=>{
+            if(e.target.className.includes('modal')) showModalFunc(e.target.id)
+        }
+    })
+
     // document.getElementsByClassName('side_button')[2].click();
 }
